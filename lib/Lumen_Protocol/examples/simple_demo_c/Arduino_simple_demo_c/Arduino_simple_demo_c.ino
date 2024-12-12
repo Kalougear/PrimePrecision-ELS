8e2dac9aecb1225d9dc5c32ecb5c94cb8713d433
sktech <stefos941@gmail.com>
2024-12-12 18:09:40 +0200

    Proculus lib

---
+++ lib/Lumen_Protocol/examples/simple_demo_c/Arduino_simple_demo_c/Arduino_simple_demo_c.ino
@@ -0,0 +1,38 @@
+#include "LumenProtocol.h"
+
+extern "C" void lumen_write_bytes(uint8_t *data, uint32_t length) {
+  Serial.write(data, length);
+}
+
+extern "C" uint16_t lumen_get_byte() {
+  if (Serial.available()) {
+    return Serial.read();
+  }
+  return DATA_NULL;
+}
+
+lumen_packet_t valor_2 = { 20, kS32};
+const int valor_1_address = 10;
+lumen_packet_t *currentPacket;
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.print("\r\n Begin");
+
+  delay(2000);
+}
+
+void loop() {
+
+  while (lumen_available() > 0) {
+    currentPacket = lumen_get_first_packet();
+
+    if (currentPacket != NULL) {
+      if (currentPacket->address == valor_1_address){
+        valor_2.data._s32 = currentPacket->data._s32;
+        lumen_write_packet(&valor_2);
+      }
+    }
+  }
+}
